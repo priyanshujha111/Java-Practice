8e19a60187f82a33be8ec85c95b84605607e72ff
Priyanshu jha <97400315+priyanshujha111@users.noreply.github.com>
2024-05-17 12:22:43 +0530

    Add files via upload

---
+++ odd even.java
@@ -0,0 +1,17 @@
+public class oddeven {
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        int number= sc.nextint(); 
+        if (number%2==0){
+            System.out.println("EVEN");
+        }
+        else{
+            System.out.println("ODD");45
+            56
+            
+        }
+    }
+
+    
+    
+}
