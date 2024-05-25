bbf13cd48b8bd665e9a6d5c2bea525ba2585064f
Priyanshu jha <97400315+priyanshujha111@users.noreply.github.com>
2024-05-25 11:30:04 +0530

    Add files via upload

---
+++ if else.java
@@ -0,0 +1,12 @@
+public class ifelse{
+    public static void main(String args[]){
+        int age=22;
+        if(age>=18){
+            System.out.println("adult");
+
+        }
+        else{
+            System.out.println("not adult");
+        }
+    }
+}
