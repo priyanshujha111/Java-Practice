9bc82277fec940be932226292d99f955923c1801
Priyanshu jha <97400315+priyanshujha111@users.noreply.github.com>
2024-11-07 23:15:07 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,13 @@
+public class array {
+    public static void main(String[] args) {
+        int a =  19;
+        String name = "Priyanshu Jha";
+        int rno1 = 23;
+        int rno2 = 55;
+        int rno3 = 18;
+        int[] rnos = new int[5];
+        int[] rnos2 = {23,34,45,89,88};
+
+    }
+    
+}
