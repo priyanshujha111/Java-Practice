2f4b894431b546da6a47febb32131c8f279d6198
Priyanshu jha <97400315+priyanshujha111@users.noreply.github.com>
2024-11-09 22:06:36 +0530

    Add files via upload

--- swap.java
+++ swap.java
@@ -1,11 +1,17 @@
 public class swap {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
-        int temp = a;
-        a = b;
-        b = temp;
-        System.out.println(a + " " + b);
-    }
+        int[][] arr = {
+            {1, 2, 3, 4},
+            {5, 6},
+            {7, 8, 9}
+        }; 
+        for (int row = 0; row < arr.length; row++){
+            for (int col = 0; col < arr[row].length; col++){
+                System.out.print(arr[row][col] + " ");
+            }
+            System.out.println();
+        }
     
+    }
 }
+
