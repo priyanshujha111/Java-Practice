67fb7150c7399e30e84d997ec95995d85e3c45d5
Priyanshu jha <97400315+priyanshujha111@users.noreply.github.com>
2024-11-23 23:04:26 +0530

    Add files via upload

---
+++ S.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+; public class S {
+    public static void main(String[] args){
+        
+        Scanner in = new Scanner(System.in);
+
+        String a = "Kunal";
+        String b = "Kunal";
+        System.out.println(a == b);
+    }
+    
+}
