42be3a64fbfc2d3e848034977ab07bdaac58914c
Priyanshu jha <97400315+priyanshujha111@users.noreply.github.com>
2025-03-13 23:45:03 +0530

    Add files via upload

--- loop.java
+++ loop.java
@@ -1,13 +1,10 @@
-import java.util.Scanner;
+
+
 public class loop {
     public static void main(String[] args) {
-       Scanner in = new Scanner(System.in);
-       int n  = in.nextInt();
-
-       for (int num = 1; num <=n; num++){
-        System.out.println("Hello world");
-       }
+        for(int num= 0; num<11; num++){
+            System.out.println(num);
+        }
     }
-
     
 }
