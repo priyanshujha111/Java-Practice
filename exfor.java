c2243b59fc6f9e9e4c90936fa8e34ef62958a87a
Priyanshu jha <97400315+priyanshujha111@users.noreply.github.com>
2025-05-11 17:02:33 +0530

    Add files via upload

---
+++ exfor.java
@@ -0,0 +1,10 @@
+public class exfor {
+    public static void main(String[] args) {
+        for(int i=1; i<=5; i++){
+            System.out.println("iteration:" + i);
+        }
+    }
+    
+}
+    
+
