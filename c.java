4574313df7167fb6561924b2d149cc09ec59e76e
Priyanshu jha <97400315+priyanshujha111@users.noreply.github.com>
2026-01-19 22:05:27 +0530

    Add files via upload

---
+++ c.java
@@ -0,0 +1,13 @@
+
+
+public class c {
+    public static void main(String[] args) {
+        int age=22;
+        if(age>=18){
+            System.out.println("Adult");
+        }
+        
+        else{
+            System.out.println("child");
+        }
+    }}
