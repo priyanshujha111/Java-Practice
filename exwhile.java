022fc6c8529bb32249d55ffd81c926351a29a260
Priyanshu jha <97400315+priyanshujha111@users.noreply.github.com>
2026-02-12 22:01:27 +0530

    Add files via upload

---
+++ exwhile.java
@@ -0,0 +1,10 @@
+public class exwhile {
+    public static void main(String[] args) {
+        int i=1;
+        while(i<=5){
+            System.out.println("iteration:" +i);
+            i++;
+        }
+    }
+    
+}
