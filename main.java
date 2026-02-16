6351184191fcf6880c5555d744cb497f37da8341
Priyanshu jha <97400315+priyanshujha111@users.noreply.github.com>
2026-02-16 19:55:40 +0530

    Add files via upload

--- main.java
+++ main.java
@@ -3,12 +3,16 @@ import java.util.Scanner;
 public class main {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String fruit = in.next();
-        String a = "Priyanshu";
-        String b = "Priyanshu";
-        
-        System.out.println(a == b);
+        Scanner sc = new Scanner(System.in);
+      int range= sc.nextInt();
+      int counter=1;
+      while(counter<=range){
+        System.out.print(counter+"");
+        counter++;
+        System.out.println("");
+
+      }
+
              
         }
         }
